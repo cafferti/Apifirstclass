343d5208b260aac4738b34d0af0b6a9a3e87cc22
cafferti <saulkumven@gmail.com>
2023-08-23 10:43:33 +0100

    fetch

--- callbacks.js
+++ callbacks.js
@@ -1,35 +1,49 @@
-//callback function
+// //callback function
 
-function firstfunction(param, callback){
-     callback()
-}
-
-
-//promises is used for what callback is used for
-
-//promisees
-//3states: pending,fulfilled, rejecting
-
-const mypromise = new Promise((resolve, reject) => {
-    const error = false
-    if(!error){
-        resolve(`yes resolve the promise`)
-    }else{
-        reject(`no! rejected the promise`)
-    }
-});
+// function firstfunction(param, callback){
+//      callback()
+// }
 
 
- const nextpromise = new Promise((resolve, reject) => {
-    setTimeout(function(){
-           resolve(`my nextpromise resolved`);
-    }, 3000)
- })
+// //promises is used for what callback is used for
 
-  nextpromise.then((value) =>{
-    console.log(value)
-  })
+// //promisees
+// //3states: pending,fulfilled, rejecting
 
-  mypromise.then((value) =>{
-    console.log(value)
+// const mypromise = new Promise((resolve, reject) => {
+//     const error = false
+//     if(!error){
+//         resolve(`yes resolve the promise`)
+//     }else{
+//         reject(`no! rejected the promise`)
+//     }
+// });
+
+
+//  const nextpromise = new Promise((resolve, reject) => {
+//     setTimeout(function(){
+//            resolve(`my nextpromise resolved`);
+//     }, 3000)
+//  })
+
+//   nextpromise.then((value) =>{
+//     console.log(value)
+//   })
+
+//   mypromise.then((value) =>{ 
+//     console.log(value)
+//   })      
+  
+  //pending state of promisse
+  const user = fetch(`https:jsonplaceholder.typicode.com/users`);
+  console.log(user);
+  //the above is the pending state of promises
+
+  fetch(`https:jsonplaceholder.typicode.com/users`)
+  .then(response =>{
+    // console.log(response)
+   return response.json();
+  }) 
+  .then(data => {
+    console.log(data)
   })
